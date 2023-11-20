494e80e8c0d6edb8a24344877a71650e115beae6
z5325779 <z5325779@ad.unsw.edu.au>
2023-11-20 16:30:04 +1100

    added homepage and search stub

---
+++ screens/HomePage.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const HomePage = () => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#1A1A1A',
+  }
+});
